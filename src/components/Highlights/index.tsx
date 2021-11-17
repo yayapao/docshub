9f6e54ff6264fcfc5c78e51cc84f4b5d50bc2cf0
Y-lonelY <luckyyoung666@gmail.com>
2021-11-17 23:11:39 +0800

    improve: cloudserver

--- src/components/Highlights/index.tsx
+++ src/components/Highlights/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
 
 export const HighlightWithCode: React.FC<{ bgColor: string }> = ({
   children,
@@ -14,4 +14,25 @@ export const HighlightWithCode: React.FC<{ bgColor: string }> = ({
   >
     {children}
   </code>
+)
+
+export const HighlightWithText: React.FC<{ bgColor?: string, style?: CSSProperties }> = ({
+  children,
+  bgColor = '#7385e4',
+  style = {}
+}) => (
+  <span
+    style={{
+      display: 'inline-block',
+      backgroundColor: bgColor,
+      borderRadius: '2px',
+      color: '#fff',
+      padding: '.2rem .3rem',
+      wordSpacing: '1.5px',
+      marginBottom: 'var(--ifm-leading)',
+      ...style
+    }}
+  >
+    {children}
+  </span>
 )

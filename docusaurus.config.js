66142b7f739d0f2fa8371a3c4a97faf6fc400e8e
Y-lonelY <luckyyoung666@gmail.com>
2021-11-22 18:41:10 +0800

    improve: config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,15 +1,31 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
+// see: https://docusaurus.io/docs/api/docusaurus-config for details
 
 const lightCodeTheme = require('prism-react-renderer/themes/github')
 const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
+// 顶栏公告信息
+const announcementBar = {
+  // id: 'support_us',
+  // content:
+  //   'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
+  // backgroundColor: '#fafbfc',
+  // textColor: '#091E42',
+  // isCloseable: true,
+}
+
 /** @type {import('@docusaurus/types').ThemeConfig} */
 const themeConfig = {
+  colorMode: {
+    respectPrefersColorScheme: true,
+  },
+  // announcementBar,
   navbar: {
-    title: 'UPDoc',
+    title: 'Docshub',
+    hideOnScroll: true,
     logo: {
-      alt: 'My Site Logo',
+      alt: 'Docs Hub',
       src: 'img/logo.svg',
     },
     items: [
@@ -21,14 +37,18 @@ const themeConfig = {
           {
             label: 'Server',
             to: '/docs/cloudServer/',
+            // 设置高亮
+            activeBaseRegex: '/docs/server'
           },
           {
             label: 'Browser',
             to: '/docs/browser/corevitals',
+            activeBaseRegex: '/docs/browser'
           },
           {
             label: 'Library',
             to: '/docs/library/nodejs',
+            activeBaseRegex: '/docs/library'
           },
         ],
       },
@@ -39,32 +59,44 @@ const themeConfig = {
           {
             label: 'JavaScript',
             to: '/docs/langs/javascript/concept',
+            activeBaseRegex: '/docs/langs/javascript'
           },
           {
             label: 'TypeScript',
             to: '/docs/langs/typescript/concept',
+            activeBaseRegex: '/docs/langs/typescript'
           },
           {
             label: 'Node.js',
             to: '/docs/langs/nodejs/concept',
+            activeBaseRegex: '/docs/langs/nodejs'
           },
           {
             label: 'Python3',
             to: '/docs/langs/python/concept',
+            activeBaseRegex: '/docs/langs/python'
           },
           {
             label: 'Golang',
             to: '/docs/langs/go/concept',
+            activeBaseRegex: '/docs/langs/go'
           },
           {
             label: 'HTML5',
             to: '/docs/langs/html5/concept',
+            activeBaseRegex: '/docs/langs/html5'
           },
           {
             label: 'CSS3',
             to: '/docs/langs/css3/concept',
+            activeBaseRegex: '/docs/langs/html5'
           },
         ],
+      },
+      {
+        label: 'Repo',
+        position: 'right',
+        to: '/docs/repo/',
       },
       {
         href: 'https://github.com/Y-lonelY',
@@ -77,6 +109,7 @@ const themeConfig = {
     style: 'dark',
     copyright: `Copyright © ${new Date().getFullYear()} 粤ICP备20070108号`,
   },
+  // code block highlight
   prism: {
     theme: lightCodeTheme,
     darkTheme: darkCodeTheme,
@@ -84,18 +117,22 @@ const themeConfig = {
 }
 
 /** @type {import('@docusaurus/types').Config} */
-const config = {
-  // 控制首页和 title
-  title: 'UP!Doooc',
+const metaConfig = {
+  title: 'Docshub',
+  // main page slogan
   tagline: 'Nice to doc, nice to code!',
-  url: 'https://your-docusaurus-test-site.com',
+  organizationName: 'Y-lonelY', // Usually your GitHub org/user name.
+  projectName: 'docshub', // Usually your repo name.
+  url: 'https://docs.7k7k.life/',
   baseUrl: '/',
+  favicon: '/img/favicon.png',
+}
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  ...metaConfig,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.png',
-  organizationName: 'Y-lonelY', // Usually your GitHub org/user name.
-  projectName: 'updoc', // Usually your repo name.
-
   presets: [
     [
       '@docusaurus/preset-classic',
@@ -103,10 +140,15 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          // editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
         },
         blog: {
+          postsPerPage: 3,
+          blogSidebarCount: 'ALL',
+          blogSidebarTitle: 'All docshub posts',
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:

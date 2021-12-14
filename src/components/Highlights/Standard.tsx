8037778e9b3dce667088a064837d2a76cda0bb0c
Y-lonelY <luckyyoung666@gmail.com>
2021-12-14 22:09:59 +0800

    improve: stanard width style

--- src/components/Highlights/Standard.tsx
+++ src/components/Highlights/Standard.tsx
@@ -12,22 +12,35 @@ export const VitalStandard: React.FC<VitalsSrandardProps> = ({
   if (!good && !improve && !bad) {
     throw new Error('good、improve or bad is required')
   }
+  const g = good && 1 || 0
+  const i = improve && 1 || 0
+  const b = bad && 1 || 0
+  const width = 100 / (g + i + b)
   return (
     <div className={styles.standardContent}>
       <span className={styles.standardLabel}>{label}</span>
       <div className={styles.standardVitals}>
         {good && (
-          <section className={clsx(styles.vitalsBox, styles.good)}>
+          <section
+            className={clsx(styles.vitalsBox, styles.good)}
+            style={{ width: `${width}%` }}
+          >
             {good}
           </section>
         )}
         {improve && (
-          <section className={clsx(styles.vitalsBox, styles.improve)}>
+          <section
+            className={clsx(styles.vitalsBox, styles.improve)}
+            style={{ width: `${width}%` }}
+          >
             {improve}
           </section>
         )}
         {bad && (
-          <section className={clsx(styles.vitalsBox, styles.bad)}>
+          <section
+            className={clsx(styles.vitalsBox, styles.bad)}
+            style={{ width: `${width}%` }}
+          >
             {bad}
           </section>
         )}

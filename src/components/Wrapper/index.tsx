0e7eb5d6796c35a30c6bd19fb36c3e030d58ce74
Y-lonelY <luckyyoung666@gmail.com>
2022-09-16 18:40:42 +0800

    feat: add githubHeader display

---
+++ src/components/Wrapper/index.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+
+export const StyledCardWrapper = styled('div')(() => ({
+  position: 'relative',
+  padding: '.5rem .75rem',
+  boxShadow: '0 2px 11px 0 rgba(190 202 218 / 17%)',
+  border: '1px solid #f0f0f0',
+  borderRadius: '6px',
+  transition: 'border .3s ease',
+  backgroundColor: '#fff',
+}))

c79425c0bd02a87f1301751e86223d83a0b64448
yayapao <luckyyoung666@gmail.com>
2024-03-22 17:47:54 +0800

    feat: refactor Table

---
+++ src/components/material/Table.tsx
@@ -0,0 +1,76 @@
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableBody,
+  styled,
+} from '@mui/material'
+
+type SentryIngestProcessorTableProps = {
+  data: {
+    columns: {
+      align?: 'center' | 'left' | 'right' | 'inherit' | 'justify'
+      name: string
+      label?: string
+    }[]
+    rows: Record<string, any>[]
+  }
+  // 是否需要展示 index, 如果为数字则代表起始值
+  index?: number
+}
+
+export const CommonTable: React.FC<SentryIngestProcessorTableProps> = ({
+  data,
+  index,
+}) => {
+  const { columns, rows } = data
+  return (
+    <StyledTableContainer component={Paper}>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            {Number.isInteger(index) && (
+              <TableCell align="left">{'index'}</TableCell>
+            )}
+            {columns.map((item) => (
+              <TableCell align={item.align || 'left'}>
+                {item.label || item.name}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, idx) => (
+            <TableRow key={idx}>
+              {Number.isInteger(index) && (
+                <TableCell align="left">{index + idx}</TableCell>
+              )}
+              {columns.map((item) => {
+                return (
+                  <TableCell key={item.name} align={item.align}>
+                    {row[item.name] || '-'}
+                  </TableCell>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </StyledTableContainer>
+  )
+}
+
+const StyledTableContainer = styled(TableContainer)<any>(() => {
+  return {
+    marginBottom: '1rem',
+    '& table': {
+      marginBottom: 0,
+    },
+    '& .MuiTableCell-root': {
+      fontSize: '0.675rem',
+    },
+  }
+})

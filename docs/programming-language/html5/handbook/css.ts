c75e2725d02b877c30ae5ee7a413b7394a09d5e7
yayapao <luckyyoung666@gmail.com>
2024-07-02 10:08:27 +0800

    feat: add table rules

---
+++ docs/programming-language/html5/handbook/css.ts
@@ -0,0 +1,39 @@
+export const infoIcon = {
+  background:
+    'url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiI+PGcgZmlsbD0iIzg4OCI+PHBhdGggZD0iTTkgN0g2djFoMXY0aDEuOTE3bDEtMUg5ek03IDRoMnYySDd6Ii8+PHBhdGggZD0iTTggMWE3IDcgMCAxMDAgMTRBNyA3IDAgMDA4IDF6bTAgMTIuNWMtMy4wMzMgMC01LjUtMi40NjctNS41LTUuNVM0Ljk2NyAyLjUgOCAyLjVzNS41IDIuNDY3IDUuNSA1LjUtMi40NjcgNS41LTUuNSA1LjV6Ii8+PC9nPjwvc3ZnPg==)',
+  width: '16px',
+  height: '16px',
+  display: 'inline-block',
+}
+
+export const filterIcon = {
+  background:
+    'url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiI+PHBhdGggZmlsbD0iIzg4OCIgZD0iTTEzIDRIM2w0IDR2NWwyLTFWOHoiLz48L3N2Zz4=)',
+  width: '16px',
+  height: '16px',
+  display: 'inline-block',
+}
+
+export const sortIcon = {
+  background:
+    'url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiI+PHBhdGggZmlsbD0iIzg4OCIgZD0iTTkgN2gzTDggMyA0IDdoM3YySDRsNCA0IDQtNEg5eiIvPjwvc3ZnPg==)',
+  width: '16px',
+  height: '16px',
+  display: 'inline-block',
+}
+
+export const sortAscIcon = {
+  background:
+    'url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiI+PHBhdGggZmlsbD0iIzQ0NCIgZD0iTTEyIDdMOCAzIDQgN2gzdjZoMlY3eiIvPjwvc3ZnPg==)',
+  width: '16px',
+  height: '16px',
+  display: 'inline-block',
+}
+
+export const sortDescIcon = {
+  background:
+    'url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiI+PHBhdGggZmlsbD0iIzQ0NCIgZD0iTTkgOVYzSDd2Nkg0bDQgNCA0LTR6Ii8+PC9zdmc+)',
+  width: '16px',
+  height: '16px',
+  display: 'inline-block',
+}

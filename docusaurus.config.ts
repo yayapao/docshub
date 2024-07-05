b1a04deb6284ed6c627d1a4daff6e7785745bc01
yayapao <luckyyoung666@gmail.com>
2024-07-05 17:40:33 +0800

    feat: update typesense

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,14 +19,7 @@ const config: Config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  plugins: [
-    // [
-    //   'docusaurus-lunr-search',
-    //   {
-    //     languages: ['en', 'zh'], // language codes
-    //   },
-    // ],
-  ],
+  plugins: [],
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you

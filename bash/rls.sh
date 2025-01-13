40ed52e423f18c16573a38606b0bcc46725742d7
yayapao <luckyyoung666@gmail.com>
2025-01-13 18:06:17 +0800

    feat: change to new server

--- bash/rls.sh
+++ bash/rls.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
 VERSION="1.0.0"
-REMOTE_ADDRESS="124.222.19.90"
-DOCS_HUB_PATH="/root/apps/docshub"
-PWD_PATH="/usr/local/etc/apps-config/apps.conf"
+# DOCS_HUB_PATH="/root/apps/docshub"
+PWD_PATH="/usr/local/etc/kk.conf"
+
+# set global variables
+password=""
+host=""
+docs_hub_path=""
+
+get_conf() {
+    config=$(cat "$PWD_PATH")
+    password=$(echo "$config" | grep PASSWD= | awk -F= '{print $2}')
+    host=$(echo "$config" | grep HOST= | awk -F= '{print $2}')
+    base_dir=$(echo "$config" | grep DIR= | awk -F= '{print $2}')
+    docs_hub_path="$base_dir/docshub"
+}
 
 build() {
     cd /Users/yango/github/react/docshub || exit
@@ -17,6 +29,7 @@ build() {
 }
 
 send() {
+    get_conf
     build
     name="dist-$(date +"%m%d%Y").tar.gz"
     read -r -p "[Warning] Current name of result is $name, please check it.\n" new_name
@@ -25,24 +38,34 @@ send() {
     fi
     echo -e "[INFO] Start sending...\n"
     tar -cvf "$name" ./dist
-    config=$(cat "$PWD_PATH")
-    password=$(echo "$config" | grep common= | awk -F= '{print $2}')
     # Send the result to server and remove local tar file
-    sshpass -p "$password" scp "$name" "root@$REMOTE_ADDRESS:$DOCS_HUB_PATH/releases"
+    sshpass -p "$password" scp "$name" "root@$host:$docs_hub_path/release"
     rm -f "$name"
     rm -rf ./dist
     echo "[INFO] Congratulations, sending task has been done!"
 }
 
+# update bash/rls.sh and upload to server
+update_bash() {
+    get_conf
+    echo "[INFO] Start updating bash/rls.sh..."
+    sshpass -p "$password" scp ./bash/rls.sh "root@$host:$docs_hub_path/rls.sh"
+    echo "[INFO] Congratulations, updating task has been done."
+}
+
 # Used to deploy the app on server
 deploy() {
-    if [ -d "$DOCS_HUB_PATH/live" ]; then
-        rm -rf "$DOCS_HUB_PATH/live"
+    base_path="/root/apps/docshub"
+    target_path="$base_path/dist"
+    if [ -d "$target_path" ]; then
+        rm -rf "$target_path"
     fi
-    mkdir "$DOCS_HUB_PATH/live"
+    mkdir "$target_path"
     # select target file
-    files="$(ls -t $DOCS_HUB_PATH/releases/)"
+    files="$(ls -t $base_path/release/)"
+
     echo "Please select the target file:"
+
     select file in $files; do
         if [ -n "$file" ]; then
             echo "You have selected $file"
@@ -52,19 +75,10 @@ deploy() {
         fi
     done
     # unzip the target file
-    tar -xvf "$DOCS_HUB_PATH/releases/$file" -C "$DOCS_HUB_PATH/live"
-    echo -e "[INFO] $file content has been placed at $DOCS_HUB_PATH/live.\n"
+    tar -xvf "$base_path/release/$file" -C "$target_path"
+    echo -e "[INFO] $file content has been placed at $target_path.\n"
     # restart the nginx
     nginx -s reload
-}
-
-# update bash/rls.sh and upload to server
-update_bash() {
-    echo "[INFO] Start updating bash/rls.sh..."
-    config=$(cat "$PWD_PATH")
-    password=$(echo "$config" | grep common= | awk -F= '{print $2}')
-    sshpass -p "$password" scp ./bash/rls.sh "root@$REMOTE_ADDRESS:$DOCS_HUB_PATH/rls.sh"
-    echo "[INFO] Congratulations, updating task has been done."
 }
 
 main() {
